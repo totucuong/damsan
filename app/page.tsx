355d176db975a4134841bf3cb303608bd6a305cc
Tu Cuong To <cuong@epsilico.com>
2025-09-01 15:36:29 +0200

    add button and textarea shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <main>this is the main chat ui</main>
+      <Button>Click me</Button>
       <footer>this is the footer</footer>
     </div>
   );
