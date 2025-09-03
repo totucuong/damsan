11a812544c94f4f6ed025d365c4669cccb7a2e07
Tu Cuong To <cuong@epsilico.com>
2025-09-03 11:04:22 +0200

    add Todo

--- components/Memory.tsx
+++ components/Memory.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { loadMemory } from "../lib/db";
 import { ChatMessage } from "./ChatMessage";
 
 // a memory is a list of messages
 export function Memory({ userId }: { userId: string }) {
+    // @TODO: load memory from the database and using
+    // streaming: https://nextjs.org/docs/app/getting-started/fetching-data#streaming
     const memory = loadMemory(userId);
     return (
         <div className="max-w-4xl mx-auto">
