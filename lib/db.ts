cafed352eaeb4c3ef03857467be27b0437b2cff0
Tu Cuong To <cuong@epsilico.com>
2025-09-05 17:34:34 +0200

    add save message to database

--- lib/db.ts
+++ lib/db.ts
@@ -5,11 +5,32 @@ export interface Message {
     message: string;
     isTyping: boolean;
 }
-// const memory: { [userId: string]: Message[] } = {
-//     "user_1":
-//         [{ "isUser": true, "message": "Which medicines have I taken today?", isTyping: false },
-//         { "isUser": false, "message": "you have taken paracetamol", isTyping: false },]
-// };
+
+
+
+const prisma = new PrismaClient();
+
+
+export async function saveMessage(message: Message, userId: string) {
+    await prisma.message.create({
+        data: {
+            message: message.message,
+            type: message.isUser ? "HUMAN" : "BOT",
+            userId: userId
+        }
+    });
+}
+
+export async function saveAllMessages(messages: Message[], userId: string) {
+    await prisma.message.createMany({
+        data: messages.map((message) => ({
+            message: message.message,
+            type: message.isUser ? "HUMAN" : "BOT",
+            userId: userId
+        }))
+    });
+}
+
 
 const memory: Record<string, Message[]> = {
     "user_1":
