a8a9810bfe923ce611252162ea155b0c234fce88
Tu Cuong To <totucuong@gmail.com>
2025-09-10 13:15:33 +0200

    remove unused code

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,7 +1,3 @@
-// import { createClient } from "@supabase/supabase-js";
-
-// const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,
-// process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!);
 'use server'
 import { createClient } from '@/utils/supabase/server'
 
