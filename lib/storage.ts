fed9c7c26691f157fdb484fddf834d6d0d41296f
Tu Cuong To <totucuong@gmail.com>
2025-09-10 12:56:33 +0200

    upload file to folder configured via env

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,7 +1,7 @@
 // import { createClient } from "@supabase/supabase-js";
 
 // const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!);
+// process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!);
 'use server'
 import { createClient } from '@/utils/supabase/server'
 
@@ -14,7 +14,7 @@ export async function uploadFile(file: File) {
         throw new Error('User must be authenticated to upload files')
     }
 
-    const { data, error } = await supabase.storage.from('damsan').upload(`${user.id} / ${file.name}`, file)
+    const { data, error } = await supabase.storage.from(process.env.SUPABASE_BUCKET!).upload(`${user.id} / ${file.name}`, file)
     if (error) {
         console.error('Failed to upload file:', error)
     } else {
