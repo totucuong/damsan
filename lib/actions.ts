f41f5e9b54f69cc9c9d19f1a6c0ba421b565e2f2
Tu Cuong To <totucuong@gmail.com>
2025-09-17 11:02:54 +0200

    change name: lib/image.ts -> lib/document.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,47 +1,57 @@
-'use server'
+"use server";
 
-import { loadMessages, saveMessages as dbSaveMessages, Message, loadProfile } from "./db";
+import {
+  loadMessages,
+  saveMessages as dbSaveMessages,
+  Message,
+  loadProfile,
+} from "./db";
 import { uploadFile as dbUploadFile } from "./storage";
-import { analyzeImageFile } from "./image";
-
+import { analyzeImageFile } from "./document";
 
 export async function getMessages(userId: string) {
-    return loadMessages(userId);
+  return loadMessages(userId);
 }
 
 export async function getProfile(userId: string) {
-    return loadProfile(userId);
+  return loadProfile(userId);
 }
 
 export async function saveMessages(messages: Message[], userId: string) {
-    return dbSaveMessages(messages, userId);
+  return dbSaveMessages(messages, userId);
 }
 
 export async function uploadFile(file: File) {
-    return dbUploadFile(file);
+  return dbUploadFile(file);
 }
 
 export async function analyzeFiles(files: File[]) {
-    return Promise.all(files.map(async (file) => {
-        return analyzeImageFile(file);
-    }));
+  return Promise.all(
+    files.map(async (file) => {
+      return analyzeImageFile(file);
+    })
+  );
 }
 
-export async function processUserMessage(message: Message, userId: string, selectedFiles?: File[]): Promise<Message[]> {
-    let aiResponse: Message[] = [];
-    try {
-        if (selectedFiles && selectedFiles?.length > 0) {
-            const results = await analyzeFiles(selectedFiles);
-            aiResponse = results.map(result => ({
-                isUser: false,
-                message: result.description,
-                isTyping: false,
-                timestamp: (new Date()),
-            }));
-        }
-        await saveMessages([message, ...aiResponse], userId);
-    } catch (error) {
-        console.error('Failed to process user message:', error);
+export async function processUserMessage(
+  message: Message,
+  userId: string,
+  selectedFiles?: File[]
+): Promise<Message[]> {
+  let aiResponse: Message[] = [];
+  try {
+    if (selectedFiles && selectedFiles?.length > 0) {
+      const results = await analyzeFiles(selectedFiles);
+      aiResponse = results.map((result) => ({
+        isUser: false,
+        message: result.description,
+        isTyping: false,
+        timestamp: new Date(),
+      }));
     }
-    return aiResponse;
+    await saveMessages([message, ...aiResponse], userId);
+  } catch (error) {
+    console.error("Failed to process user message:", error);
+  }
+  return aiResponse;
 }
