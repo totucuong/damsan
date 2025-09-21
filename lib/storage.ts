c5dfda48934071a4c6cb8c061b7577db752b30c4
Tu Cuong To <totucuong@gmail.com>
2025-09-21 13:34:03 +0200

    fix path construction

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,18 +1,22 @@
-'use server'
-import { createClient } from '@/utils/supabase/server'
+"use server";
+import { createClient } from "@/utils/supabase/server";
 
 export async function uploadFile(file: File) {
-    const supabase = await createClient()
-    // Check if user is authenticated
-    const { data: { user } } = await supabase.auth.getUser()
+  const supabase = await createClient();
+  // Check if user is authenticated
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    if (!user) {
-        throw new Error('User must be authenticated to upload files')
-    }
-
-    const { data, error } = await supabase.storage.from(process.env.SUPABASE_BUCKET!).upload(`${user.id} / ${file.name}`, file)
-    if (error) {
-        throw new Error('Failed to upload file: ' + error.message)
-    }
-    return data
+  if (!user) {
+    throw new Error("User must be authenticated to upload files");
+  }
+  const path = [user.id, file.name].join("/");
+  const { data, error } = await supabase.storage
+    .from(process.env.SUPABASE_BUCKET!)
+    .upload(path, file);
+  if (error) {
+    throw new Error("Failed to upload file: " + error.message);
+  }
+  return data;
 }
